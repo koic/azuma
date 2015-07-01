7bc05539e85f2f25972e1b9b0d4e2a32416b8c43
Koichi ITO <koic.ito@gmail.com>
2015-07-01 22:41:14 +0900

    DRY, see README.md

--- lib/russian_roulette.rb
+++ lib/russian_roulette.rb
@@ -1,16 +1,3 @@
-#
-# ```ruby
-# talkers = %w(
-#   岩本
-#   杉野
-#   馬場
-# )
-#
-# roulette = RussianRoulette.entry(talkers)
-#
-# roulette.next_talker
-# ```
-#
 require 'russian_roulette/announce'
 require 'russian_roulette/core'
 
